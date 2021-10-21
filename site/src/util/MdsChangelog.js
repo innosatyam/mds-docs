0deddfdabdcf9581903cb6dc01d1bb52a2a37501
Anuradha aggarwal <anuradha.mcs18.du@gmail.com>
2021-10-21 23:44:47 +0530

    fix(homepage): fix homepage design issues

--- site/src/util/MdsChangelog.js
+++ site/src/util/MdsChangelog.js
@@ -1,11 +1,11 @@
 import { useStaticQuery, graphql } from 'gatsby';
 
-export function MdsChangelog() {
+export default function MdsChangelog() {
   const {
     allMdx: { edges },
   } = useStaticQuery(graphql`
     query MDS_CHANGELOG {
-      allMdx(filter: {frontmatter: {title: {eq: "Release Note"}}}) {
+      allMdx(filter: {frontmatter: {title: {eq: "Release notes"}}}) {
         edges {
           node {
             internal {
@@ -22,31 +22,34 @@ export function MdsChangelog() {
 
   const items = edges.map(({ node }) => node);
 
-  const metadata = items[0].headings[0].value;
-
-  const version = metadata.substring(0, metadata.indexOf('('));
-  const releaseDate = metadata.substring(
-    metadata.indexOf("(") + 1,
-    metadata.lastIndexOf(")")
-  );
-
   const record = {
-    version: version,
-    releaseDate: releaseDate,
+    version: '',
+    releaseDate: '',
     updatesList: []
   }
 
-  const htmlContent = items[0].internal.content;
-  const latestUpdate = htmlContent.split('\n----')[0];
+  if (items.length > 0) {
 
-  let updateList = latestUpdate.split('###');
+    const metadata = items[0].headings[0].value;
 
-  const listSeparator = '*';
+    record.version = metadata.substring(0, metadata.indexOf('('));
+    record.releaseDate = metadata.substring(
+      metadata.indexOf("(") + 1,
+      metadata.lastIndexOf(")")
+    );
 
-  updateList.shift();
-  updateList
-    .filter((item) => item.includes(listSeparator))
-    .map((item) => record.updatesList.push(item.split(listSeparator)));
+    const htmlContent = items[0].internal.content;
+    const latestUpdate = htmlContent.split('\n----')[0];
 
+    let updateList = latestUpdate.split('###');
+
+    const listSeparator = '*';
+
+    updateList.shift();
+    updateList
+      .filter((item) => item.includes(listSeparator))
+      .map((item) => record.updatesList.push(item.split(listSeparator)));
+
+  }
   return record;
 }
