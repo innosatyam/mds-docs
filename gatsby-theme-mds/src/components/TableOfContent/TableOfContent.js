5aa33286d2f061c6fc8460b595c04037939a8470
Anuradha aggarwal <anuradha.mcs18.du@gmail.com>
2021-11-26 21:27:04 +0530

    fix(TableOfContent): fix tableOfContent scroll behaviour

--- gatsby-theme-mds/src/components/TableOfContent/TableOfContent.js
+++ gatsby-theme-mds/src/components/TableOfContent/TableOfContent.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useNavItems } from '../../util/InPageNavItems';
 import { Subheading } from '@innovaccer/design-system';
 import { Link } from 'gatsby';
@@ -6,52 +6,97 @@ import './TableOfContent.css';
 
 const TableOfContent = (props) => {
   const { relativePagePath, location } = props;
+  const clickedRef = useRef(false);
+  const unsetClickedRef = useRef(null);
+
   let navItems = useNavItems(relativePagePath);
-  const [active, setActive] = React.useState('');
+  const [active, setActive] = useState('');
 
-  let tocLevelCount = 1;
-
-  const onClickHandler = (itemUrl) => {
-    setActive(itemUrl);
-  };
+  useEffect(() => {
+    const navIds = getIds(navItems);
+    document.body.addEventListener('scroll', (e) => onScrollHandler(e, navIds), true);
+    return () => {
+      document.body.removeEventListener('scroll', (e) => onScrollHandler(e, navIds), true);
+    }
+  }, []);
 
   useEffect(() => {
     let urlHash = '';
     if (location && location.hash) {
-      urlHash = location.hash;
+      urlHash = location.hash.slice(1);
       setActive(urlHash);
     } else {
-      navItems && navItems.length ? setActive(navItems[0].url) : '';
+      navItems && navItems.length ? setActive(navItems[0].url.slice(1)) : '';
     }
   }, []);
 
-  function renderItems(items, isChild = false) {
+  const onScrollHandler = (e, idList) => {
+    // Don't set the active index based on scroll if a link was just clicked
+    if (clickedRef.current) {
+      return;
+    }
+    const headerHeight = document.getElementById('mainHeader').getBoundingClientRect().height;
+    const viewportHeight = document.body.offsetHeight - headerHeight;
+    let resultFound = false;
+
+    idList && idList.forEach((item) => {
+      let element = document.getElementById(item);
+      if (element && !resultFound) {
+        const { top, height } = element.getBoundingClientRect();
+        if (top > 0 && viewportHeight > top - height) {
+          setActive(item);
+          resultFound = true;
+        };
+      };
+    });
+  };
+
+  const onClickHandler = (e, itemUrl) => {
+    setActive(itemUrl);
+
+    // Used to disable onScrollHandler if the page scrolls due to a click
+    clickedRef.current = true;
+    unsetClickedRef.current = setTimeout(() => {
+      clickedRef.current = false;
+    }, 1000);
+  };
+
+  function getIds(items) {
+    return items.reduce((acc, item) => {
+      if (item.url) {
+        // url has a # as first character, remove it to get the raw Id
+        acc.push(item.url.slice(1));
+      }
+      if (item.items) {
+        acc.push(...getIds(item.items));
+      }
+      return acc;
+    }, []);
+  }
+
+  function renderItems(items) {
     return (
       <ul className='table-of-content-list pr-8'>
-        {items.map((item) => {
+        {items && items.map((item) => {
           return (
             <li>
               <div
-                className={`${
-                  active == item.url ? 'active-link' : ''
-                }`}
+                className={`${active == item.url.slice(1) ? 'active-link' : ''
+                  }`}
               >
                 <Link
                   to={item.url}
-                  onClick={() => onClickHandler(item.url)}
+                  onClick={(e) => onClickHandler(e, item.url.slice(1))}
                   className='toc-link'
                   style={{
-                    display: `${
-                      item.title ? 'inline-block' : 'none'
-                    }`,
+                    display: `${item.title ? 'inline-block' : 'none'
+                      }`,
                   }}
                 >
                   {item.title}
                 </Link>
               </div>
-              {item.items &&
-                (tocLevelCount++,
-                renderItems(item.items, true))}
+              {item.items && renderItems(item.items)}
             </li>
           );
         })}
